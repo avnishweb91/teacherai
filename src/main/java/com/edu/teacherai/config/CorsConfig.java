b2884eac0cc445c31aa1135512c492de225db47b
amacshri <avnishweb91@gamil.com>
2026-04-24 02:18:30 +0530

    fix: add api.smartboard.co.in to CORS allowed origins
    Co-Authored-By: Avnish <avnishweb91@gmail.com>

--- src/main/java/com/edu/teacherai/config/CorsConfig.java
+++ src/main/java/com/edu/teacherai/config/CorsConfig.java
@@ -22,7 +22,8 @@ public class CorsConfig implements Filter {
         "http://localhost:5173",
         "http://localhost:3000",
         "https://smartboard.co.in",
-        "https://www.smartboard.co.in"
+        "https://www.smartboard.co.in",
+        "https://api.smartboard.co.in"
     );
 
     @Override
